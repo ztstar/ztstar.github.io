a64bb0f59b96a1598253b4d3e1bdc2b0a395ac75
ztstar <zoutong2006@gmail.com>
2025-07-21 22:08:19 -0700

    set base to be my-repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ztstar.github.io/",
-	base: "",
+	base: "my-repo",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({

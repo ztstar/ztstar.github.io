2efb03fe077d84ffef3420e786d85fb2ad91563e
ztstar <zoutong2006@gmail.com>
2026-06-14 20:44:42 -0400

    minor change

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "ztstar",
-	subtitle: "Demo Site",
+	title: "ztstar's blog",
+	subtitle: "",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -39,43 +39,43 @@ export const siteConfig: SiteConfig = {
 	],
 };
 
-export const navBarConfig: NavBarConfig = {
-	links: [
-		LinkPreset.Home,
-		LinkPreset.Archive,
-		LinkPreset.About,
-		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		},
-	],
-};
+// export const navBarConfig: NavBarConfig = {
+// 	links: [
+// 		LinkPreset.Home,
+// 		LinkPreset.Archive,
+// 		LinkPreset.About,
+// 		{
+// 			name: "GitHub",
+// 			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+// 			external: true, // Show an external link icon and will open in a new tab
+// 		},
+// 	],
+// };
 
-export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "ztstar",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
-		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
-		},
-	],
-};
+// export const profileConfig: ProfileConfig = {
+// 	avatar: "assets/images/hachiware.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+// 	name: "ztstar",
+// 	bio: "In delay there lies no plenty. ",
+// 	links: [
+// 		{
+// 			name: "Twitter",
+// 			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+// 			// You will need to install the corresponding icon set if it's not already included
+// 			// `pnpm add @iconify-json/<icon-set-name>`
+// 			url: "https://twitter.com",
+// 		},
+// 		{
+// 			name: "Steam",
+// 			icon: "fa6-brands:steam",
+// 			url: "https://store.steampowered.com",
+// 		},
+// 		{
+// 			name: "GitHub",
+// 			icon: "fa6-brands:github",
+// 			url: "https://github.com/saicaca/fuwari",
+// 		},
+// 	],
+// };
 
 export const licenseConfig: LicenseConfig = {
 	enable: true,

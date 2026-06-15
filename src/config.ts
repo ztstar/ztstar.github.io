1b8227814bf634871222536be4cad71fa492ee8d
ztstar <zoutong2006@gmail.com>
2026-06-14 20:57:40 -0400

    minor change

--- src/config.ts
+++ src/config.ts
@@ -39,43 +39,43 @@ export const siteConfig: SiteConfig = {
 	],
 };
 
-// export const navBarConfig: NavBarConfig = {
-// 	links: [
-// 		LinkPreset.Home,
-// 		LinkPreset.Archive,
-// 		LinkPreset.About,
-// 		{
-// 			name: "GitHub",
-// 			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-// 			external: true, // Show an external link icon and will open in a new tab
-// 		},
-// 	],
-// };
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		// {
+		// 	name: "GitHub",
+		// 	url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+		// 	external: true, // Show an external link icon and will open in a new tab
+		// },
+	],
+};
 
-// export const profileConfig: ProfileConfig = {
-// 	avatar: "assets/images/hachiware.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-// 	name: "ztstar",
-// 	bio: "In delay there lies no plenty. ",
-// 	links: [
-// 		{
-// 			name: "Twitter",
-// 			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-// 			// You will need to install the corresponding icon set if it's not already included
-// 			// `pnpm add @iconify-json/<icon-set-name>`
-// 			url: "https://twitter.com",
-// 		},
-// 		{
-// 			name: "Steam",
-// 			icon: "fa6-brands:steam",
-// 			url: "https://store.steampowered.com",
-// 		},
-// 		{
-// 			name: "GitHub",
-// 			icon: "fa6-brands:github",
-// 			url: "https://github.com/saicaca/fuwari",
-// 		},
-// 	],
-// };
+export const profileConfig: ProfileConfig = {
+	avatar: "assets/images/hachiware.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "ztstar",
+	bio: "In delay there lies no plenty. ",
+	links: [
+		// {
+		// 	name: "Twitter",
+		// 	icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+		// 	// You will need to install the corresponding icon set if it's not already included
+		// 	// `pnpm add @iconify-json/<icon-set-name>`
+		// 	url: "https://twitter.com",
+		// },
+		// {
+		// 	name: "Steam",
+		// 	icon: "fa6-brands:steam",
+		// 	url: "https://store.steampowered.com",
+		// },
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/saicaca/fuwari",
+		},
+	],
+};
 
 export const licenseConfig: LicenseConfig = {
 	enable: true,
